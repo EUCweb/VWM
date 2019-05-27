0a236c193a74601df4599c83fce6ccec6a6e0825
JimMoyle <github@jimmoyle.com>
2019-05-27 02:16:00 +0100

    revert comment change

--- VWM.ps1
+++ VWM.ps1
@@ -376,7 +376,7 @@ $wpf.dgUser.add_SelectedCellsChanged( {
                 Invoke-Item $logPath
             }
             else {
-                #Hopefully sensible error
+                #Hopefully sensible error message
                 Write-Error "Log file at $logPath could not be found in the same directory as the related JSON file."
             }
             
